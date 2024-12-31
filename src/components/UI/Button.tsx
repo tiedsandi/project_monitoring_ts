fee73bf607c317361e376d9120c72ce253b4d333
tiedsandi <fachransandi@gmail.com>
2024-12-31 19:56:23 +0700

    add Session Page

--- src/components/UI/Button.tsx
+++ src/components/UI/Button.tsx
@@ -1,13 +1,32 @@
 import type {ComponentPropsWithoutRef, ReactNode} from 'react';
+import {Link, type LinkProps} from 'react-router-dom';
 
 type BaseProps = {
   children: ReactNode;
   textOnly?: boolean;
 };
 
-type ButtonProps = ComponentPropsWithoutRef<'button'> & BaseProps;
+type ButtonProps = ComponentPropsWithoutRef<'button'> & BaseProps & {to?: never};
+type ButtonLinkProps = LinkProps & BaseProps & {to: string};
 
-export default function Button(props: ButtonProps) {
+function isRouterLink(
+  props: ButtonProps | ButtonLinkProps
+): props is ButtonLinkProps {
+  return 'to' in props;
+}
+
+export default function Button(props: ButtonProps | ButtonLinkProps) {
+  if (isRouterLink(props)) {
+    const {children, textOnly, ...otherProps} = props;
+    return (
+      <Link
+        className={`button ${textOnly ? 'button--text-only' : ''}`}
+        {...otherProps}>
+        {children}
+      </Link>
+    );
+  }
+
   const {children, textOnly, ...otherProps} = props;
 
   return (
